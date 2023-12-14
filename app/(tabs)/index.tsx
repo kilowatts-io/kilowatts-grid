ee0c3fb59536cc2314f98b3d9638f332a8428a44
ben@kilowatts.io <ben@kilowatts.io>
2023-12-14 21:40:47 +0000

    remove starter code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,6 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-import { FavouritesList } from '../../components/Favourites';
 import { GeneratorLive } from '../../components/GeneratorLive';
 
 export default function TabOneScreen() {
   return       <GeneratorLive/>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
