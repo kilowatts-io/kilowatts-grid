099bfd02b3f87024c524541117d878b496376231
ben@kilowatts.io <ben@kilowatts.io>
2023-12-18 15:21:06 +0000

    add eas update and github actions

--- index.tsx
+++ index.tsx
@@ -8,9 +8,33 @@ import { useFonts } from "expo-font";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { ThemeProvider } from "@rneui/themed";
 import { WithLicense } from "./components/WithLicense";
+import * as Updates from "expo-updates";
+import { AppState, AppStateStatus } from "react-native";
+import log from "./services/log";
 
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 2000);
+
+/*
+onFetchUpdateAsync() checks for updates from expo and downloads them if available.
+*/
+async function onFetchUpdateAsync() {
+  log.debug("Checking for Expo updates...");
+  try {
+    const update = await Updates.checkForUpdateAsync();
+    if (update.isAvailable) {
+      log.debug("Downloading Expo update...");
+      await Updates.fetchUpdateAsync();
+      log.debug("Reloading app...");
+      await Updates.reloadAsync();
+    } else {
+      log.debug("No Expo update available.");
+    }
+  } catch (error) {
+    // You can also add an alert() to see the error message in case of an error when fetching updates.
+    log.error(`Error fetching latest Expo update: ${error}`);
+  }
+}
 
 export const App = () => {
   //   React.useEffect(() => {
@@ -19,6 +43,26 @@ export const App = () => {
   //       rt();
   //     }
   //   }, []);
+
+  // on initial load, check for updates
+  React.useEffect(() => {
+    if (!__DEV__) {
+      onFetchUpdateAsync();
+    }
+  }, []);
+
+  // on resuming from background, check for updates
+  React.useEffect(() => {
+    const handleAppStateChange = (state: AppStateStatus) => {
+      if (state === "active" && !__DEV__) {
+        onFetchUpdateAsync();
+      }
+    };
+    AppState.addEventListener("change", handleAppStateChange);
+    return () => {
+      (AppState as any).removeEventListener("change", handleAppStateChange);
+    };
+  }, []);
 
   const [loaded, error] = useFonts({
     SpaceMono: require("./assets/fonts/SpaceMono-Regular.ttf"),
--- services/log.ts
+++ services/log.ts
@@ -3,6 +3,7 @@
 export const log = {
     debug: (msg: string) => {},
     info: (msg: string) => console.log(msg),
+    error: (msg: string) => console.error(msg),
 }
 
 export default log
