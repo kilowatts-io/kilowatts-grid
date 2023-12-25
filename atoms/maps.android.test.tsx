a9bc249657b8a5a67e1ce06e7dbf90af8c9665d5
Ben Watts <ben@kilowatts.io>
2023-12-25 05:36:52 +0000

    remove redudant test file

--- atoms/maps.android.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react-native";
-import * as m from "./maps.android";
-
-describe("atoms/maps.android/UnitGroupMap", () => {
-  test("renders undefined as maps not supported on android yet", () => {
-    render(<m.UnitGroupMap />);
-    expect(screen.debug()).toBe(undefined);
-  });
-});
