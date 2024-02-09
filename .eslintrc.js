58bedb2c60b0d23b36c7303e0f40572a82cbf4d2
David Leuliette <dleuliette@gmail.com>
2024-02-09 08:13:13 +0100

    setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  parser: "@typescript-eslint/parser",
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+};
