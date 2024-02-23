ac93a43d1b9417982ac13ac2f1d24822f6ae8e79
Ben Watts <ben@kilowatts.io>
2024-02-23 08:21:04 +0000

    remove version code from web

---
+++ components/gb-live/version-info.web.tsx
@@ -0,0 +1,8 @@
+// text compoment which renders version information from expo-updates
+import React from "react";
+
+const VersionInfo: React.FC = () => {
+  return null;
+};
+
+export default VersionInfo;
