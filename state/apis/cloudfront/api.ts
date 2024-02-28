764d14cf1b52466b96b1dccce08a4aa24d894dfa
Ben Watts <ben@kilowatts.io>
2024-02-28 12:30:53 +0000

    rollback

--- state/apis/cloudfront/api.ts
+++ state/apis/cloudfront/api.ts
@@ -8,7 +8,7 @@ import { GbSummaryOutputResponse } from "./types";
 export const kilowattsCloudfront = createApi({
   reducerPath: "kilowattsCloudfront",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://gridcdn.kilowatts.io`
+    baseUrl: `https://${EXPO_PUBLIC_CDN_DOMAIN_NAME}`
   }),
   endpoints: (builder) => ({
     gbSummaryOutput: builder.query<GbSummaryOutputResponse, void>({
