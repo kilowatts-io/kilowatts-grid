dfd64049b8e1ea6e2d4ce52a9a90be885deefc42
Ben Watts <ben@kilowatts.io>
2024-09-06 14:16:18 +0100

    add skia

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,21 @@
 // create a hello world page
 
-import React from 'react';
-import {Text} from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+import SkiaExampleComponent from "../components/skia-example";
 
 const HelloWorld: React.FC = () => {
-    return (
-        <Text>hello</Text>
-    )
-}
-
-export default HelloWorld
+  return (
+    <View style={styles.container}>
+      <SkiaExampleComponent/>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default HelloWorld;
