af493269fd9497222c378904aa31020ccb20c36c
Ben Watts <ben@kilowatts.io>
2024-09-10 20:32:54 +0100

    switch to cdn

--- index.web.tsx
+++ index.web.tsx
@@ -1,9 +1,11 @@
 import '@expo/metro-runtime';
 import { App } from 'expo-router/build/qualified-entry';
 import { renderRootComponent } from 'expo-router/build/renderRootComponent';
-
 import { LoadSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
+import { version } from 'canvaskit-wasm/package.json';
 
-LoadSkiaWeb().then(async () => {
+LoadSkiaWeb({
+  locateFile: (file) => `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${version}/bin/full/${file}`
+}).then(async () => {
   renderRootComponent(App);
 });
