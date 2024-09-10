68543fb3a629ffb23f2577c7dc900082d35090cd
Ben Watts <ben@kilowatts.io>
2024-09-10 22:14:12 +0100

    add scroll to unit group

--- src/components/unit-group-card.tsx
+++ src/components/unit-group-card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, List } from "react-native-paper";
 import { formatMW } from "./icon-list-item";
+import { ScrollView } from "react-native";
 
 interface UnitGroupAccordionProps {
   title: string;
@@ -28,34 +29,42 @@ const capitalise = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
 export const UnitGroupCard: React.FC<AppListIconProps> = (p) => {
   return (
-    <Card>
-      <Card.Content>
-        <List.Section>
-          <UnitGroupAccordion title="Type" content={capitalise(p.fuel_type)} />
-          <UnitGroupAccordion
-            title="Actual Output"
-            content={formatMW(p.output.level)}
-          />
-          <UnitGroupAccordion title="Capacity" content={formatMW(p.capacity)} />
-          <UnitGroupAccordion
-            title="Capacity Factor"
-            content={renderCapacityFactor(p.capacityFactor)}
-          />
-          <UnitGroupAccordion
-            title="Ramp Rate / minute"
-            content={formatMW(p.output.delta)}
-          />
-          <UnitGroupAccordion
-            title="Scheduled (PN - pre balancing) Output"
-            content={formatMW(p.output.level - p.balancing_volume)}
-          />
-          <UnitGroupAccordion
-            title="Balancing Market Volume"
-            content={formatMW(p.balancing_volume)}
-          />
-        </List.Section>
-      </Card.Content>
-    </Card>
+    <ScrollView>
+      <Card>
+        <Card.Content>
+          <List.Section>
+            <UnitGroupAccordion
+              title="Type"
+              content={capitalise(p.fuel_type)}
+            />
+            <UnitGroupAccordion
+              title="Actual Output"
+              content={formatMW(p.output.level)}
+            />
+            <UnitGroupAccordion
+              title="Capacity"
+              content={formatMW(p.capacity)}
+            />
+            <UnitGroupAccordion
+              title="Capacity Factor"
+              content={renderCapacityFactor(p.capacityFactor)}
+            />
+            <UnitGroupAccordion
+              title="Ramp Rate / minute"
+              content={formatMW(p.output.delta)}
+            />
+            <UnitGroupAccordion
+              title="Scheduled (PN - pre balancing) Output"
+              content={formatMW(p.output.level - p.balancing_volume)}
+            />
+            <UnitGroupAccordion
+              title="Balancing Market Volume"
+              content={formatMW(p.balancing_volume)}
+            />
+          </List.Section>
+        </Card.Content>
+      </Card>
+    </ScrollView>
   );
 };
 
