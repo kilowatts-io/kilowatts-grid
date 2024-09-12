5a36d36585e75a0baf90d7af7ae403a7a23e8395
Ben Watts <ben@kilowatts.io>
2024-09-12 07:56:01 +0100

    remove sentry from dev

--- src/utils/sentry/index.web.ts
+++ src/utils/sentry/index.web.ts
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/react'
 
 export const initialise = () => {
+  if(__DEV__) return;
   Sentry.init({dsn: "https://3c939b287c552ff45b9d35217091ef2b@o4505890996748288.ingest.us.sentry.io/4506466906734592"})
 };
 
 export const errorHandler = (error: Error, stackTrace: string) => {
+  if(__DEV__) return;
   Sentry.captureException(error);
 };
